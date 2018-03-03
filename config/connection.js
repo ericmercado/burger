e09fa7f960fe83fd6697e95919162b833199cb50
Eric <eric.mercado60@hotmail.com>
2018-03-02 22:07:09 -0600

    Final

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,12 @@
 var mysql = require('mysql');
 
-// Create the MySQL connection object
 var connection;
 
-if (process.env.heroku_URL) {
-  // DB is JawsDB on Heroku
-  connection = mysql.createConnection(process.env.heroku_URL);
+if (process.env.JAWSDB_URL) {
+
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  // DB is local on localhost
+
   connection = mysql.createConnection({
     port: 3306,
     host: 'localhost',
@@ -17,7 +16,6 @@ if (process.env.heroku_URL) {
   })
 };
 
-// Make the connection to MySQL
 connection.connect(function(err) {
   if (err) {
     console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
@@ -26,5 +24,4 @@ connection.connect(function(err) {
   console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
 });
 
-// Export connection for ORM use
 module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,5 @@
 var connection = require ('./connection.js');
 
-// Helper function for generating MySQL syntax
 function printQuestionMarks(num) {
 	var arr = [];
 
@@ -11,7 +10,6 @@ function printQuestionMarks(num) {
 	return arr.toString();
 }
 
-// Helper function for generating My SQL syntax
 function objToSql(ob) {
 	var arr = [];
 
@@ -22,27 +20,23 @@ function objToSql(ob) {
 	return arr.toString();
 }
 
-// Create the ORM object to perform SQL queries
 var orm = {
-	// Function that returns all table entries
+
 	selectAll: function(tableInput, cb) {
-		// Construct the query string that returns all rows from the target table
+
 		var queryString = "SELECT * FROM " + tableInput + ";";
 
-		// Perform the database query
 		connection.query(queryString, function(err, result) {
 			if (err) {
 				throw err;
 			}
 
-			// Return results in callback
 			cb(result);
 		});
 	},
 
-	// Function that insert a single table entry
 	insertOne: function(table, cols, vals, cb) {
-		// Construct the query string that inserts a single row into the target table
+
 		var queryString = "INSERT INTO " + table;
 
 		queryString += " (";
@@ -52,22 +46,17 @@ var orm = {
 		queryString += printQuestionMarks(vals.length);
 		queryString += ") ";
 
-		// console.log(queryString);
-
-		// Perform the database query
 		connection.query(queryString, vals, function(err, result) {
 			if (err) {
 				throw err;
 			}
 
-			// Return results in callback
 			cb(result);
 		});
 	},
 
-	// Function that updates a single table entry
 	updateOne: function(table, objColVals, condition, cb) {
-		// Construct the query string that updates a single entry in the target table
+
 		var queryString = "UPDATE " + table;
 
 		queryString += " SET ";
@@ -75,19 +64,14 @@ var orm = {
 		queryString += " WHERE ";
 		queryString += condition;
 
-		// console.log(queryString);
-
-		// Perform the database query
 		connection.query(queryString, function(err, result) {
 			if (err) {
 				throw err;
 			}
 
-			// Return results in callback
 			cb(result);
 		});
 	}
 };
 
-// Export the orm object for use in other modules
 module.exports = orm;
